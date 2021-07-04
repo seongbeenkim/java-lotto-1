26336c6489f309fd626abb5d6a7e7de865ba84df
Chloe <page.prologue@gmail.com>
2021-07-04 19:32:54 +0900

    feat : 로또 번호 범위 유효성 검사 추가

--- src/main/java/lotto/LottoNumber.java
+++ src/main/java/lotto/LottoNumber.java
@@ -3,10 +3,20 @@ package lotto;
 import java.util.Objects;
 
 public class LottoNumber {
+    public static final int MIN_LOTTO_BOUND = 1;
+    public static final int MAX_LOTTO_BOUND = 45;
+
     private final int lottoNumber;
 
     public LottoNumber(final int lottoNumber) {
+        validateBound(lottoNumber);
         this.lottoNumber = lottoNumber;
+    }
+
+    private void validateBound(int lottoNumber) {
+        if (lottoNumber < MIN_LOTTO_BOUND || lottoNumber > MAX_LOTTO_BOUND) {
+            throw new IllegalArgumentException("로또 번호는 1 ~ 45 까지 입니다.");
+        }
     }
 
     public int value() {
--- src/test/java/lotto/LottoNumberTest.java
+++ src/test/java/lotto/LottoNumberTest.java
@@ -1,7 +1,10 @@
 package lotto;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -15,5 +18,15 @@ class LottoNumberTest {
 
         //when //then
         assertThat(lottoNumber).isEqualTo(new LottoNumber(1));
+    }
+
+    @ParameterizedTest()
+    @ValueSource(ints = {0, 46})
+    @DisplayName("로또 번호가 1 ~ 45이 아닐 경우 예외가 발생한다")
+    void validate_lotto_number(int invalidLottoNumber) {
+        //given //when //then
+        Assertions.assertThatIllegalArgumentException()
+                .isThrownBy(() -> new LottoNumber(invalidLottoNumber))
+                .withMessage("로또 번호는 1 ~ 45 까지 입니다.");
     }
 }
