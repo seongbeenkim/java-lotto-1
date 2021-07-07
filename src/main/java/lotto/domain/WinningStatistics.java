d1f40bcd3279e8309dae26b817a6f566fa881e2a
Chloe <page.prologue@gmail.com>
2021-07-07 16:40:40 +0900

    feat : 당첨 번호 개수가 같은 로또 티켓 개수 반환 테스트 및 기능 구현

--- src/main/java/lotto/domain/WinningStatistics.java
+++ src/main/java/lotto/domain/WinningStatistics.java
@@ -1,11 +1,30 @@
 package lotto.domain;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 import static lotto.domain.LottoTicketVendingMachine.TICKET_PRICE;
 
 public class WinningStatistics {
+    private final WinningNumbers winningNumbers;
 
-    public int matchedCount(LottoTicket lottoTickets, WinningNumbers winningNumbers) {
-        return winningNumbers.matchedWinningNumberCount(lottoTickets);
+    public WinningStatistics(WinningNumbers winningNumbers) {
+        this.winningNumbers = winningNumbers;
+    }
+
+    private List<Integer> matchedCountList(List<LottoTicket> lottoTickets) {
+        return lottoTickets.stream()
+                .map(lottoTicket -> winningNumbers.matchedWinningNumberCount(lottoTicket))
+                .collect(Collectors.toList());
+    }
+
+    public Map<Integer, Long> groupByWinningNumber(List<LottoTicket> lottoTickets) {
+        List<Integer> matchedCounts = matchedCountList(lottoTickets);
+
+        return matchedCounts.stream()
+                .collect(Collectors.groupingBy(n -> n, HashMap::new, Collectors.counting()));
     }
 
     public float profitRate(int ticketAmount, int totalPrize) {
--- src/test/java/lotto/WinningStatisticsTest.java
+++ src/test/java/lotto/WinningStatisticsTest.java
@@ -1,15 +1,19 @@
 package lotto;
 
 import lotto.domain.LottoPrize;
+import lotto.domain.LottoTicket;
 import lotto.domain.WinningNumbers;
 import lotto.domain.WinningStatistics;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -22,6 +26,22 @@ public class WinningStatisticsTest {
         List<Integer> inputNumbers = Arrays.asList(1, 2, 3, 4, 5, 6);
         int bonusNumber = 45;
         return new WinningNumbers(inputNumbers, bonusNumber);
+    }
+
+    @Test
+    @DisplayName("당첨 번호 개수가 같은 로또 티켓 개수를 반환한다")
+    void group_by_matched_count() {
+        //given
+        ArrayList<LottoTicket> lottoTickets = generateLottoTicketList();
+
+        //when
+        Map<Integer, Long> matchedCountStatistic = winningStatistics.groupByWinningNumber(lottoTickets);
+
+        //then
+        assertThat(matchedCountStatistic.get(3)).isEqualTo(1);
+        assertThat(matchedCountStatistic.get(4)).isEqualTo(1);
+        assertThat(matchedCountStatistic.get(5)).isNull();
+        assertThat(matchedCountStatistic.get(6)).isNull();
     }
 
     @ParameterizedTest
@@ -38,6 +58,17 @@ public class WinningStatisticsTest {
 
         //then
         assertThat(profit).isEqualTo(expectedProfit);
+    }
+
+    private ArrayList<LottoTicket> generateLottoTicketList() {
+        ArrayList<LottoTicket> lottoTicketList = new ArrayList<>();
+        lottoTicketList.add(new LottoTicket(3, 4, 5, 6, 7, 8)); // 4개 일치
+        lottoTicketList.add(new LottoTicket(4, 5, 6, 7, 8, 9)); // 3개 일치
+        lottoTicketList.add(new LottoTicket(5, 6, 7, 8, 9, 10)); // 2개 일치
+        lottoTicketList.add(new LottoTicket(6, 7, 8, 9, 10, 11)); // 1개 일치
+        lottoTicketList.add(new LottoTicket(7, 8, 9, 10, 11, 12)); // 0개 일치
+        lottoTicketList.add(new LottoTicket(8, 9, 10, 11, 12, 13)); //0개 일치
+        return lottoTicketList;
     }
 
     private static Stream<Arguments> generateProfit() {
