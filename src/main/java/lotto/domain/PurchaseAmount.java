13cccd9cedd8cbf11d832a3f3fb8aed7d0fa6e52
Beenie93 <seongbeen93@naver.com>
2021-07-12 00:29:40 +0900

    feat: 구입 금액이 1000원 미만일 경우에 대한 예외 테스트 코드 및 기능 추가

---
+++ src/main/java/lotto/domain/PurchaseAmount.java
@@ -0,0 +1,33 @@
+package lotto.domain;
+
+import java.util.Objects;
+
+public class PurchaseAmount {
+    private static final int PURCHASE_AMOUNT_MIN_BOUND = 1_000;
+
+    private final int purchaseAmount;
+
+    public PurchaseAmount(final int purchaseAmount) {
+        validateBoundOf(purchaseAmount);
+        this.purchaseAmount = purchaseAmount;
+    }
+
+    private void validateBoundOf(int purchaseAmount) {
+        if (purchaseAmount < PURCHASE_AMOUNT_MIN_BOUND) {
+            throw new IllegalArgumentException("최소 구입 금액은 1,000원 이상이여야 합니다.");
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        PurchaseAmount that = (PurchaseAmount) o;
+        return purchaseAmount == that.purchaseAmount;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(purchaseAmount);
+    }
+}
---
+++ src/test/java/lotto/domain/PurchaseAmountTest.java
@@ -0,0 +1,32 @@
+package lotto.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+
+public class PurchaseAmountTest {
+
+    @Test
+    @DisplayName("구매 금액을 생성한다.")
+    void create() {
+        //given
+        int paidMoney = 1000;
+
+        //when
+        PurchaseAmount purchaseAmount = new PurchaseAmount(paidMoney);
+
+        //then
+        assertThat(purchaseAmount).isEqualTo(new PurchaseAmount(paidMoney));
+    }
+
+    @Test
+    @DisplayName("1,000원 미만일 경우, 예외가 발생한다.")
+    void validate() {
+        //given //when //then
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> new PurchaseAmount(999))
+                .withMessage("최소 구입 금액은 1,000원 이상이여야 합니다.");
+    }
+}
