069aa0f74a6009eaba2416eea568fe73f0cc0e50
Beenie93 <seongbeen93@naver.com>
2021-07-13 19:55:24 +0900

    feat: 로또 티켓 장수가 1장 미만일 경우 예외 발생 테스트 코드 및 기능 구현

--- src/main/java/lotto/domain/NumberOfTickets.java
+++ src/main/java/lotto/domain/NumberOfTickets.java
@@ -3,10 +3,19 @@ package lotto.domain;
 import java.util.Objects;
 
 public class NumberOfTickets {
+    private static final int NUMBER_OF_TICKETS_MIN_BOUND = 1;
+
     private final int numberOfTickets;
 
     public NumberOfTickets(final int numberOfTickets) {
+        validatePositive(numberOfTickets);
         this.numberOfTickets = numberOfTickets;
+    }
+
+    private void validatePositive(final int numberOfTickets) {
+        if (numberOfTickets < NUMBER_OF_TICKETS_MIN_BOUND) {
+            throw new IllegalArgumentException("티켓은 1장 이상이여야 합니다.");
+        }
     }
 
     public int value() {
--- src/test/java/lotto/domain/NumberOfTicketsTest.java
+++ src/test/java/lotto/domain/NumberOfTicketsTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 public class NumberOfTicketsTest {
 
@@ -18,5 +19,14 @@ public class NumberOfTicketsTest {
 
         //then
         assertThat(numberOfTickets).isEqualTo(new NumberOfTickets(number));
+    }
+
+    @Test
+    @DisplayName("1장 미만일 경우, 예외가 발생한다.")
+    void validatePositive() {
+        //given //when //then
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> new NumberOfTickets(0))
+                .withMessage("티켓은 1장 이상이여야 합니다.");
     }
 }
