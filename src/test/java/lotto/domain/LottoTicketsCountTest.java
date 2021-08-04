cb9ab124c24da21b5b246d188af49273a3ea9d6a
Beenie93 <seongbeen93@naver.com>
2021-08-04 22:35:50 +0900

    fix: 테스트 케이스 실패 수정, 1장 미만 -> 0장 미만

--- src/test/java/lotto/domain/LottoTicketsCountTest.java
+++ src/test/java/lotto/domain/LottoTicketsCountTest.java
@@ -25,11 +25,11 @@ public class LottoTicketsCountTest {
     }
 
     @Test
-    @DisplayName("1장 미만일 경우, 예외가 발생한다.")
+    @DisplayName("0장 미만일 경우, 예외가 발생한다.")
     void validatePositive() {
         //when //then
         assertThatIllegalArgumentException()
-                .isThrownBy(() -> new LottoTicketsCount(0))
+                .isThrownBy(() -> new LottoTicketsCount(-1))
                 .withMessage("티켓은 0장 이상이여야 합니다.");
     }
 
